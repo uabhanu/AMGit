72ebcb3778133181c09414e91f0934a2f29c4c6d
Bhanu <uabhanu@gmail.com>
2017-12-07 18:33:19 +0000

    Lecture 290 Up Next

---
+++ Ahoy Matey/Assets/Managers/BhanuNetworkManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BhanuNetworkManager : NetworkManager
+{   
+    public void BhanuStartHost()
+    {
+       StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Host Started");
+    }
+}
