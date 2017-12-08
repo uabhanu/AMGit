289d9452dc9417b11f2aa8c4b5cb5bfd53f56ffe
Bhanu <uabhanu@gmail.com>
2017-12-08 11:46:45 +0000

    Lecture 290 WIP

---
+++ AhoyMatey/Assets/Managers/BhanuNetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BhanuNetworkManager : NetworkManager
+{   
+    public void BhanuStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " : Host Start Requested");
+        StartHost();
+    }
+
+    public override void OnClientConnect(NetworkConnection bhanuClientConnection)
+    {
+        Debug.Log("Client Connected to IP : " + bhanuClientConnection.address);
+    }
+
+    public override void OnStartClient(NetworkClient bhanuClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " : Client Start Requested");
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " : Host Started");
+    }
+}
---
+++ AhoyMatey/Assets/Players/BhanuPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class BhanuPlayer : MonoBehaviour 
+{
+    Vector3 m_inputValue;
+
+	void Start() 
+    {
+		
+	}
+
+	void Update() 
+    {
+		if(Time.timeScale == 0)
+            return;
+
+            m_inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+            m_inputValue.y = 0f;
+            m_inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+            transform.Translate(m_inputValue);
+	}
+}
